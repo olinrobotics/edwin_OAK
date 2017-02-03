45046cbe8cba3d1bbc6463de6a960ce79ed0c2f2
srli <sophia.li@students.olin.edu>
2017-02-03 16:19:31 -0500

    arm_node added to correct module

---
+++ scripts/midbrain/sense/camera.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+"""
+Edwin uses usb_cam to convert usb camera feed into rostopic data feed for all the other nodes to listen to
+This code will associate camera frame information to the base of the robot
+"""
